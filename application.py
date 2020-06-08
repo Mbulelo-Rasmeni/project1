cfbd47e5400821a7cc06c644dba9202e0f0e5a37
unknown <m.s.rasmeni@gmail.com>
2020-06-08 21:41:05 +0200

    Connected to Goodreads API and created my own API for book information requests

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -88,22 +89,18 @@ def BookDetails(isbn):
 
     if book is None:
         return render_template("error.html", message="Book or Author does not exist")
-    else:
-        # Get reviews
-        reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn",{"isbn": isbn}).fetchall()
+    
+    reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn",{"isbn": isbn}).fetchall()
 
-        rating = 10
-        count = 5
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "INaco71gUTAqoo00oGsWAw", "isbns": book.isbn})
+    data = res.json()
+    print(data)
+    avgRating = data["books"][0]["average_rating"]
+    numberOfRatings = data["books"][0]["work_ratings_count"]
+    #avgRating = 2
+    #numberOfRatings = 2
 
-        
-        for review in reviews:
-            rating+= review.user_rating
-            count=count+1
-
-        avgRating = int(rating/count)
-
-
-    return render_template("BookDetails.html", book=book, reviews=reviews,rating=avgRating)
+    return render_template("BookDetails.html", book=book, reviews=reviews,rating=avgRating, num_ratings=numberOfRatings)
 
 @app.route("/review", methods=["POST"])
 def addReview():
@@ -111,20 +108,13 @@ def addReview():
     userReview = ""
     userRating = 0
 
-    print(f"Before checking book reviews")
-
     #Check if the current user has reviewed the book before
     if "user" in session:
-        print(f"Passed user_session check")
         user = session["user"]
         book_isbn = request.form["isbn"]
-        print(f"check current review using data from form {book_isbn} and {user}")
-        #checkRating = db.execute("SELECT * FROM reviews WHERE email LIKE :email AND isbn LIKE :isbn", {"email": user, "isbn":book_isbn}).rowcount == 0:
         if db.execute("SELECT * FROM reviews WHERE email LIKE :email AND isbn LIKE :isbn", {"email": user, "isbn":book_isbn}).rowcount == 0:
-            print(f"Current review is NONE")
             userReview = request.form["userReview"]
             userRating = int(request.form["userRating"])
-            print(f"User rating from form {userRating}")
             db.execute("INSERT INTO reviews (email, user_review, user_rating, isbn) VALUES (:email, :user_review, :user_rating, :isbn)",{"email": user, "user_review": userReview, "user_rating":userRating, "isbn":book_isbn})
             db.commit()
             
@@ -164,3 +154,56 @@ def logout():
     #Go back to home page
 
     return render_template("index.html")
+
+@app.route("/api/flights/<int:flight_id>")
+def flight_api(flight_id):
+    """Return details about a single flight."""
+
+    # Make sure flight exists.
+    flight = Flight.query.get(flight_id)
+    if flight is None:
+        return jsonify({"error": "Invalid flight_id"}), 422
+
+    # Get all passengers.
+    passengers = flight.passengers
+    names = []
+    for passenger in passengers:
+        names.append(passenger.name)
+    return jsonify({
+            "origin": flight.origin,
+            "destination": flight.destination,
+            "duration": flight.duration,
+            "passengers": names
+        })
+
+@app.route("/api/<string:isbn>")
+def BookAPI(isbn):
+
+    """Book information"""
+
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+
+    if book is None:
+        return jsonify({"error": "Invalid ISBN"}), 404
+    
+    # Get reviews from my database
+    reviews = db.execute("SELECT * FROM reviews WHERE isbn = :isbn",{"isbn": isbn}).fetchall()
+
+    rating = 0
+    count = 0
+
+    #Calculating average rating for a book
+    for review in reviews:
+        rating+= review.user_rating
+        count+=1
+
+    avgRating = int(rating/count)
+
+    return jsonify({
+            "title": book.title,
+            "author": book.author,
+            "year": book.year,
+            "isbn": book.isbn,
+            "review_count":count,
+            "average_score":avgRating
+        })  
